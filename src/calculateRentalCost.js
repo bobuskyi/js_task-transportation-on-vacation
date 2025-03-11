551fd23c29c1439b2f1463192efd63e46e8f14bb
bobuskyi <y.bobuskyi@gmail.com>
2025-03-11 14:03:02 +0200

    add rental calculation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const CAR_COST_PER_DAY = 40;
+  const SMALL_DISCOUNT = 20;
+  const BIG_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const LONG_TERM = 7;
+  let discount = 0;
+
+  if (days >= SHORT_TERM) {
+    discount = SMALL_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    discount = BIG_DISCOUNT;
+  }
+
+  return days * CAR_COST_PER_DAY - discount;
 }
 
 module.exports = calculateRentalCost;
